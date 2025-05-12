f41e77b38ce2fcf4a3c0145505793009133693c5
godfather2327 <harshithsai94@gmail.com>
2025-05-11 21:04:53 -0700

    chore: add initial structure

---
+++ user-service/user-service/src/test/java/com/harshith/chaosdemo/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshith.chaosdemo.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
